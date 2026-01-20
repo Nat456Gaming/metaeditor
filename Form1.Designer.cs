483f15bc33702414935b9e95b65d9ef91b515961
Lyn <nat456.pro@gmail.com>
2026-01-20 10:35:14 +0100

    Debut implémentation interface

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,46 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
+            splitContainer1 = new SplitContainer();
+            checkBox3 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox1 = new CheckBox();
+            textBox1 = new TextBox();
             label1 = new Label();
             button1 = new Button();
+            listView1 = new ListView();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
             SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.BorderStyle = BorderStyle.Fixed3D;
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(checkBox3);
+            splitContainer1.Panel1.Controls.Add(checkBox2);
+            splitContainer1.Panel1.Controls.Add(checkBox1);
+            splitContainer1.Panel1.Controls.Add(textBox1);
+            splitContainer1.Panel1.Controls.Add(label1);
+            splitContainer1.Panel1.Controls.Add(button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(listView1);
+            splitContainer1.Size = new Size(1000, 562);
+            splitContainer1.SplitterDistance = 378;
+            splitContainer1.TabIndex = 0;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(10, 117);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(179, 29);
+            checkBox3.TabIndex = 5;
+            checkBox3.Text = "Respecter la casse";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(10, 82);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(334, 29);
+            checkBox2.TabIndex = 4;
+            checkBox2.Text = "Correspondre à toutes les occurences";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(10, 47);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(295, 29);
+            checkBox1.TabIndex = 3;
+            checkBox1.Text = "Utiliser les expressions régulières";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(10, 10);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(316, 31);
+            textBox1.TabIndex = 2;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(552, 185);
+            label1.Location = new Point(10, 169);
             label1.Name = "label1";
-            label1.Size = new Size(61, 20);
-            label1.TabIndex = 0;
-            label1.Text = "Bonjour";
-            label1.Click += label1_Click;
+            label1.Size = new Size(59, 25);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            label1.Click += label1_Click_1;
             // 
             // button1
             // 
-            button1.Location = new Point(505, 92);
+            button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            button1.Location = new Point(259, 514);
             button1.Name = "button1";
-            button1.Size = new Size(172, 29);
-            button1.TabIndex = 1;
-            button1.Text = "Appuie tu verras";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 0;
+            button1.Text = "Appliquer";
             button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            button1.Click += button1_Click_1;
+            // 
+            // listView1
+            // 
+            listView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            listView1.Location = new Point(0, 0);
+            listView1.Margin = new Padding(0);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(608, 557);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
             // 
             // MetaEditor
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
-            Controls.Add(label1);
+            ClientSize = new Size(1000, 562);
+            Controls.Add(splitContainer1);
+            Margin = new Padding(4);
             Name = "MetaEditor";
             Text = "MetaEditor";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
+        private SplitContainer splitContainer1;
         private Button button1;
+        private ListView listView1;
+        private Label label1;
+        private TextBox textBox1;
+        private CheckBox checkBox3;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Collections.Specialized;
+
 namespace metaeditor
 {
     public partial class MetaEditor : Form
@@ -14,7 +16,38 @@ namespace metaeditor
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "Ok";
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            label1.Text = "bonjour";
+            listView1.Items.Add("bonjour");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
